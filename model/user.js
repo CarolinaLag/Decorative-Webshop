84bbc5d551cb4fd06a90c45888f2995bd8f55510
fredrikaericsson <fredrika@finnvid.se>
2021-03-12 15:57:09 +0100

    Created productview with onclick function for description

--- model/user.js
+++ model/user.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    role: {type: String},
-    token: String,
-  tokenExpiration: Date
-})
+  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String },
+  token: String,
+  tokenExpiration: Date,
+});
 
 const User = mongoose.model("user", userSchema);
 
