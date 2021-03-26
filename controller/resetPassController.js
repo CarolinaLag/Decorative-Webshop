78c83a93a34327da4c3a3547cc6ff39120ef6726
fredrikaericsson <fredrika@finnvid.se>
2021-03-26 09:28:02 +0100

    Small change to email

--- controller/resetPassController.js
+++ controller/resetPassController.js
@@ -1,10 +1,10 @@
-const User = require('../model/user');
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
-const bcrypt = require('bcrypt');
-const sgMail = require('@sendgrid/mail');
-var sgTransport = require('nodemailer-sendgrid-transport');
-require('dotenv').config();
+const User = require("../model/user");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
+const sgMail = require("@sendgrid/mail");
+var sgTransport = require("nodemailer-sendgrid-transport");
+require("dotenv").config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -17,7 +17,7 @@ var options = {
 var transport = nodemailer.createTransport(sgTransport(options));
 
 exports.resetRender = (req, res) => {
-  res.render('reset.ejs', { err: '' });
+  res.render("reset.ejs", { err: "" });
 };
 
 exports.resetSubmit = async (req, res) => {
@@ -25,23 +25,23 @@ exports.resetSubmit = async (req, res) => {
 
   try {
     const user = await User.findOne({ email: email });
-    if (!user) return res.render('reset.ejs', { err: 'Not a valid user' });
+    if (!user) return res.render("reset.ejs", { err: "Not a valid user" });
 
-    const token = await crypto.randomBytes(32).toString('hex');
+    const token = await crypto.randomBytes(32).toString("hex");
 
     user.token = token;
     user.tokenExpiration = Date.now() + 3600000;
     await user.save();
 
     await transport.sendMail({
-      from: process.env.USER,
+      from: process.env.USER_EMAIL,
       to: user.email,
-      subject: 'Reset password requested',
+      subject: "Reset password requested",
       html: `<h2> Click  <a href="http://localhost:8000/reset/${user.token}" > here </a> to reset the password </h2>`,
     });
-    res.render('resetMail.ejs', { err: err });
+    res.render("resetMail.ejs", { err: err });
   } catch (err) {
-    res.render('resetMail.ejs', { err: ' ' });
+    res.render("resetMail.ejs", { err: " " });
   }
 };
 
@@ -53,11 +53,11 @@ exports.resetParams = async (req, res) => {
       token: token,
       tokenExpiration: { $gt: Date.now() },
     });
-    if (!user) return res.redirect('/register');
+    if (!user) return res.redirect("/register");
 
-    res.render('resetForm.ejs', { err: '', email: user.email });
+    res.render("resetForm.ejs", { err: "", email: user.email });
   } catch (err) {
-    res.render('reset.ejs', { err: 'Please try again' });
+    res.render("reset.ejs", { err: "Please try again" });
   }
 };
 
@@ -73,8 +73,8 @@ exports.resetFormSubmit = async (req, res) => {
 
     user.password = hashedPassword;
     await user.save();
-    res.redirect('/login');
+    res.redirect("/login");
   } catch (err) {
-    res.render('resetForm.ejs', { err: err });
+    res.render("resetForm.ejs", { err: err });
   }
 };
